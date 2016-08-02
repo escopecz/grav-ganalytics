3be2179f07a4bdc78e2eff0be3524d63b9a54a54
Christian Worreschk <christian.worreschk@mccann.de>
2016-08-02 10:52:04 +0200

    Allow to anonymize the IP address of the hit sent to Google Analytics. See https://developers.google.com/analytics/devguides/collection/analyticsjs/ip-anonymization

--- ganalytics.php
+++ ganalytics.php
@@ -31,10 +31,17 @@ class GanalyticsPlugin extends Plugin
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');                
+            ";
+
+            $anomizeIp = $this->config->get('plugins.ganalytics.anonymizeIp') ? 'true' : 'false';
+            $functions = "
                 ga('create', '{$trackingId}', 'auto');
+                ga('set', 'anonymizeIp', {$anomizeIp});
                 ga('send', 'pageview');
             ";
+
+            $init.= $functions;
             $this->grav['assets']->addInlineJs($init);
         }
     }
