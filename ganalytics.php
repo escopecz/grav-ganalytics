be55775357457cd82f26a1ca65ffdb3abd6db10b
Oliver Okrongli <oliver.okrongli@infix.de>
2017-02-28 02:18:03 +0100

    Update Google's 'ga' function to use https

--- ganalytics.php
+++ ganalytics.php
@@ -67,7 +67,7 @@ class GanalyticsPlugin extends Plugin
                 "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n".
                 "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n".
                 "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n".
-                "})(window,document,'script','//www.google-analytics.com/{$scriptName}.js','{$objectName}');\n"
+                "})(window,document,'script','https://www.google-analytics.com/{$scriptName}.js','{$objectName}');\n"
             ;
         }
         return $code;
